1e9631304f3d0ff79c98d0bfb9ee2a899e459cec
julien-ctx <jdaodkql@gmail.com>
2022-11-19 15:37:42 +0100

    Basic vector OK and first usage of allocator

--- Vector.hpp
+++ Vector.hpp
@@ -6,11 +6,18 @@
 /*   By: jcauchet <jcauchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 09:56:11 by jcauchet          #+#    #+#             */
-/*   Updated: 2022/11/19 12:34:13 by jcauchet         ###   ########.fr       */
+/*   Updated: 2022/11/19 15:34:17 by jcauchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+// https://en.cppreference.com/w/cpp/memory/allocator#:~:text=The%20default%20allocator%20is%20stateless,of%20the%20same%20allocator%20type.
+// To check the type
+
+// https://medium.com/@terselich/1-a-guide-to-implement-a-simple-c-stl-allocator-705ede6b60e4
+
+// https://channgo2203.github.io/allocator/
 
 #include <string>
 #include <iostream>
@@ -25,12 +32,34 @@ public:
 	/* ----- Constructors ----- */
 
 	// Default
-	explicit Vector() {}
+	explicit Vector() : _array(NULL) {}
 	
-	explicit Vector (size_t n, const T &val = value_type()) {}
+	explicit Vector (size_t n, const T &val = Allocator::value_type()) : _size(n)
+	{
+		this->_array = this->_alloc.allocate(n);
+		for (size_t i = 0; i < n; i++)
+			this->_array[i] = val;
+	}
+
+	/* ----- Accessors ----- */
+
+	T	*getArray() const
+	{
+		return this->_array;
+	}
+
+	/* ----- Destructors ----- */
+	
+	~Vector()
+	{
+		this->_alloc.deallocate(this->_array, this->_size);
+	}
 
 private:
+	T *_array;
+	size_t _size;
+
+	Allocator _alloc;
 };
 
 }
-
--- main.cpp
+++ main.cpp
@@ -6,24 +6,24 @@
 /*   By: jcauchet <jcauchet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 10:02:51 by jcauchet          #+#    #+#             */
-/*   Updated: 2022/11/19 12:08:17 by jcauchet         ###   ########.fr       */
+/*   Updated: 2022/11/19 15:35:49 by jcauchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Vector.hpp"
 #include <vector>
+#include <limits.h>
+
+int *trash()
+{
+    ft::Vector<int> Test(5, 42);
+    return Test.getArray();
+}
 
 int main()
 {
-    int val = 1;
-    unsigned int n = 5;
- 
-    // fill constructor
-    std::vector<int> vec(n, val);
- 
-    for (int i= 0; (unsigned int)i < n; i++) {
-        std::cout << i << ' ';
-    }
- 
-    return 0;
+    int *array = trash();
+    for (int i = 0; i < 5; i++)
+        std::cout << array[i] << std::endl;
+    system("leaks ft_containers");
 }
