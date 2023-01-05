440823b4904a4bdb516344a3d85a826aad217cfd
Julien CAUCHETEUX <jdaodkql@gmail.com>
2023-01-05 23:05:30 +0100

    Added forgotten specializations for iterator_traits

--- utils/iterator_traits.hpp
+++ utils/iterator_traits.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstddef>
 #include <iostream>
 #include <string>
 
@@ -9,6 +10,14 @@
 namespace ft
 {
 
+// A simple implementation to explain the inheritance of iterators.
+// For example forward_iterator is also an input_iterator.
+struct input_iterator_tag  {};
+struct output_iterator_tag {};
+struct forward_iterator_tag       : public input_iterator_tag         {};
+struct bidirectional_iterator_tag : public forward_iterator_tag       {};
+struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+
 template<class Iter>
 struct iterator_traits
 {
@@ -17,6 +26,26 @@ struct iterator_traits
 	typedef typename Iter::iterator_category iterator_category;
 	typedef typename Iter::pointer pointer;
 	typedef typename Iter::reference reference;
+};
+
+template<class T>
+struct iterator_traits<T*>
+{
+	typedef T value_type;
+	typedef ptrdiff_t difference_type;
+	typedef ft::random_access_iterator_tag iterator_category;
+	typedef T* pointer;
+	typedef T& reference;
+};
+
+template<class T>
+struct iterator_traits<const T*>
+{
+	typedef T value_type;
+	typedef ptrdiff_t difference_type;
+	typedef ft::random_access_iterator_tag iterator_category;
+	typedef const T* pointer;
+	typedef const T& reference;
 };
 
 }
