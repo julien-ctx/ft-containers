361061b4fefd84305f4fcc179d99f0198ad531db
julien-ctx <jdaodkql@gmail.com>
2023-02-01 11:42:51 +0100

    Fixed relational operator for map

--- includes/map.hpp
+++ includes/map.hpp
@@ -504,7 +504,7 @@ const map<Key, T, Compare, Alloc> &rhs)
 template<class Key, class T, class Compare, class Alloc>
 bool operator<(const map<Key, T, Compare, Alloc> &lhs,
 const map<Key, T, Compare, Alloc> &rhs)
-{return ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.begin());}
+{return ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());}
 
 template<class Key, class T, class Compare, class Alloc>
 bool operator>(const map<Key, T, Compare, Alloc> &lhs,
--- main.cpp
+++ main.cpp
@@ -14,29 +14,46 @@
 
 
 #include "containers_test/srcs/map/common.hpp"
-#define T1 int
-#define T2 std::string
+#define T1 char
+#define T2 int
+typedef _pair<const T1, T2> T3;
 
-struct ft_more {
-        bool    operator()(const T1 &first, const T1 &second) const {
-                return (first > second);
-        }
-};
+template <class MAP>
+void    cmp(const MAP &lhs, const MAP &rhs)
+{
+        static int i = 0;
 
-typedef TESTED_NAMESPACE::map<T1, T2, ft_more> ft_mp;
-typedef TESTED_NAMESPACE::map<T1, T2, ft_more>::iterator ft_mp_it;
+        std::cout << "############### [" << i++ << "] ###############"  << std::endl;
+        std::cout << "eq: " << (lhs == rhs) << " | ne: " << (lhs != rhs) << std::endl;
+        std::cout << "lt: " << (lhs <  rhs) << " | le: " << (lhs <= rhs) << std::endl;
+        std::cout << "gt: " << (lhs >  rhs) << " | ge: " << (lhs >= rhs) << std::endl;
+}
 
 int             main(void)
 {
-        ft_mp mp;
+        TESTED_NAMESPACE::map<T1, T2> mp1;
+        TESTED_NAMESPACE::map<T1, T2> mp2;
 
-        mp[42] = "fgzgxfn";
-        mp[25] = "funny";
-        mp[80] = "hey";
-        mp[12] = "no";
-        mp[27] = "bee";
-        mp[90] = "8";
-        printSize(mp);
+        mp1['a'] = 2; mp1['b'] = 3; mp1['c'] = 4; mp1['d'] = 5;
+        mp2['a'] = 2; mp2['b'] = 3; mp2['c'] = 4; mp2['d'] = 5;
+
+        cmp(mp1, mp1); // 0
+        cmp(mp1, mp2); // 1
+
+        mp2['e'] = 6; mp2['f'] = 7; mp2['h'] = 8; mp2['h'] = 9;
+
+        cmp(mp1, mp2); // 2
+        cmp(mp2, mp1); // 3
+
+        (++(++mp1.begin()))->second = 42;
+
+        cmp(mp1, mp2); // 4
+        cmp(mp2, mp1); // 5
+
+        swap(mp1, mp2);
+
+        cmp(mp1, mp2); // 6
+        cmp(mp2, mp1); // 7
 
         return (0);
 }
