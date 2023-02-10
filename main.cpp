29b32b2b6b499fd9f20f55f38bbc426c1691cfde
Julien CAUCHETEUX <jdaodkql@gmail.com>
2023-02-10 15:06:35 +0000

    Added main to test RBT colors

--- main.cpp
+++ main.cpp
@@ -18,105 +18,27 @@
 
 #define TEST ft
 
-#include <stdlib.h>
-
-#define MAX_RAM 4294967296
-#define BUFFER_SIZE 4096
-struct Buffer
+int main()
 {
-	int idx;
-	char buff[BUFFER_SIZE];
-};
-
-
-#define COUNT (MAX_RAM / (int)sizeof(Buffer))
-
-template<typename T>
-class MutantStack : public TEST::stack<T>
-{
-public:
-	MutantStack() {}
-	MutantStack(const MutantStack<T>& src) { *this = src; }
-	MutantStack<T>& operator=(const MutantStack<T>& rhs) 
+	TEST::map<int, int> mp;
+	mp.insert(TEST::make_pair(34, 3));
+	mp.insert(TEST::make_pair(22, 3));
+	mp.insert(TEST::make_pair(10, 3));
+	mp.insert(TEST::make_pair(9, 3));
+	mp.insert(TEST::make_pair(1, 3));
+	mp.insert(TEST::make_pair(4, 3));
+	mp.insert(TEST::make_pair(30, 3));
+	mp.insert(TEST::make_pair(8, 3));
+	mp.insert(TEST::make_pair(32, 3));
+	mp.insert(TEST::make_pair(3, 3));
+	for (TEST::map<int, int>::iterator it = mp.begin(); it != mp.end(); it++)
 	{
-		this->c = rhs.c;
-		return *this;
+		if (it.getCurr()->color == RED)
+			std::cout << it->first << ": RED"<< std::endl;
+		else
+			std::cout << it->first << ": BLACK"<< std::endl;
 	}
-	~MutantStack() {}
-
-	typedef typename TEST::stack<T>::container_type::iterator iterator;
-
-	iterator begin() { return this->c.begin(); }
-	iterator end() { return this->c.end(); }
-};
-
-int main(int argc, char** argv) {
-	if (argc != 2)
-	{
-		std::cerr << "Usage: ./test seed" << std::endl;
-		std::cerr << "Provide a seed please" << std::endl;
-		std::cerr << "Count value:" << COUNT << std::endl;
-		return 1;
-	}
-	const int seed = atoi(argv[1]);
-	srand(seed);
-
-	TEST::vector<std::string> vector_str;
-	TEST::vector<int> vector_int;
-	TEST::stack<int> stack_int;
-	TEST::vector<Buffer> vector_buffer;
-	TEST::stack<Buffer, std::deque<Buffer> > stack_deq_buffer;
-	TEST::map<int, int> map_int;
-
-	for (int i = 0; i < COUNT; i++)
-	{
-		vector_buffer.push_back(Buffer());
-	}
-
-	for (int i = 0; i < COUNT; i++)
-	{
-		const int idx = rand() % COUNT;
-		vector_buffer[idx].idx = 5;
-	}
-	TEST::vector<Buffer>().swap(vector_buffer);
-
-	try
-	{
-		for (int i = 0; i < COUNT; i++)
-		{
-			const int idx = rand() % COUNT;
-			vector_buffer.at(idx);
-			std::cerr << "Error: THIS VECTOR SHOULD BE EMPTY!!" <<std::endl;
-		}
-	}
-	catch(const std::exception& e)
-	{
-		//NORMAL ! :P
-	}
-	
-	for (int i = 0; i < COUNT; ++i)
-	{
-		map_int.insert(TEST::make_pair(rand(), rand()));
-	}
-
-	int sum = 0;
-	for (int i = 0; i < 10000; i++)
-	{
-		int access = rand();
-		sum += map_int[access];
-	}
-	std::cout << "should be constant with the same seed: " << sum << std::endl;
-
-	{
-		TEST::map<int, int> copy = map_int;
-	}
-	MutantStack<char> iterable_stack;
-	for (char letter = 'a'; letter <= 'z'; letter++)
-		iterable_stack.push(letter);
-	for (MutantStack<char>::iterator it = iterable_stack.begin(); it != iterable_stack.end(); it++)
-	{
-		std::cout << *it;
-	}
-	std::cout << std::endl;
-	return (0);
+		
+	std::cout << "------------\n";
+	std::cout << mp.size() << std::endl;
 }
